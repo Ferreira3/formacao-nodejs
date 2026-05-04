08f3b149bc86354148963751b260c13199775728
José Eduardo Ferreira <54028051+Ferreira3@users.noreply.github.com>
2026-05-04 17:20:59 -0300

    Add round delay; Improve 'bout' logic and add random damage dealt

--- 03-projeto-mario-kart/src/index.js
+++ 03-projeto-mario-kart/src/index.js
@@ -14,6 +14,9 @@ do {
   player2 = players[Math.floor(Math.random() * 6)];
 } while (player1 === player2);
 
+// função para adicionar delay entre rodadas
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
 async function rollDice() {
   return Math.floor(Math.random() * 6) + 1;
 }
@@ -46,7 +49,8 @@ async function logRollResult(characterName, block, diceResult, attribute) {
 
 async function playRaceEngine(character1, character2) {
   for (let round = 1; round <= 5; round++) {
-    console.log(`🏁 Rodada ${round}`);
+    await delay(1000);
+    console.log(`🏁 RODADA ${round}🏁`);
 
     // sortear bloco
     let block = await getRandomBlock();
@@ -101,6 +105,8 @@ async function playRaceEngine(character1, character2) {
     if (block === "CONFRONTO") {
       let powerResult1 = diceResult1 + character1.PODER;
       let powerResult2 = diceResult2 + character2.PODER;
+      let damageDone = Math.floor(Math.random() * 2 + 1);
+      const damageEmoji = ["", "🐢", "💣"];
 
       console.log(`${character1.NOME} confrontou com ${character2.NOME}! 🥊`);
 
@@ -118,37 +124,36 @@ async function playRaceEngine(character1, character2) {
         character2.PODER
       );
 
-      if (powerResult1 > powerResult2 && character2.PONTOS > 0) {
-        console.log(
-          `${character1.NOME} venceu o confronto! ${character2.NOME} perdeu 1 ponto 🐢`
-        );
-        character2.PONTOS--;
+      // verificando vencedor do confronto e aplicando o dano causado
+      if (powerResult1 > powerResult2) {
+        console.log(`${character1.NOME} venceu o confronto!`);
+        console.log(`${character2.NOME} perdeu ${damageDone} ponto(s) ${damageEmoji[damageDone]}`);
+        character2.PONTOS = Math.max(0, character2.PONTOS - damageDone);
+      } else if (powerResult2 > powerResult1) {
+        console.log(`${character2.NOME} venceu o confronto!`);
+        console.log(`${character1.NOME} perdeu ${damageDone} ponto(s) ${damageEmoji[damageDone]}`);
+        character1.PONTOS = Math.max(0, character1.PONTOS - damageDone);
+      } else {
+        console.log("Confronto empatado!");
       }
-
-      if (powerResult2 > powerResult1 && character1.PONTOS > 0) {
-        console.log(
-          `${character2.NOME} venceu o confronto! ${character1.NOME} perdeu 1 ponto 🐢`
-        );
-        character1.PONTOS--;
-      }
-
-      console.log(
-        powerResult2 === powerResult1
-          ? "Confronto empatado! Nenhum ponto foi perdido"
-          : ""
-      );
     }
 
-    // verificando o vencedor
-    if (totalTestSkill1 > totalTestSkill2) {
-      console.log(`${character1.NOME} marcou um ponto!`);
-      character1.PONTOS++;
-    } else if (totalTestSkill2 > totalTestSkill1) {
-      console.log(`${character2.NOME} marcou um ponto!`);
-      character2.PONTOS++;
+    // verificando o vencedor em casos de RETA e CURVA
+    if (block !== "CONFRONTO"){
+      if (totalTestSkill1 > totalTestSkill2) {
+        console.log(`${character1.NOME} marcou um ponto!`);
+        character1.PONTOS++;
+      } else if (totalTestSkill2 > totalTestSkill1) {
+        console.log(`${character2.NOME} marcou um ponto!`);
+        character2.PONTOS++;
+      } else {
+        console.log("Empate!")
+      }
     }
 
-    console.log("-----------------------------");
+    console.log(
+      `${"-".repeat(15)}${character1.NOME} [${character1.PONTOS}]—[${character2.PONTOS}] ${character2.NOME}${"-".repeat(15)}`
+    );
   }
 }
 
@@ -158,10 +163,10 @@ async function declareWinner(character1, character2) {
   console.log(`${character2.NOME}: ${character2.PONTOS} ponto(s)`);
 
   if (character1.PONTOS > character2.PONTOS)
-    console.log(`\n${character1.NOME} venceu a corrida! Parabéns! 🏆`);
+    console.log(`\n${character1.NOME} venceu a corrida! Parabéns! 🏆`.toUpperCase());
   else if (character2.PONTOS > character1.PONTOS)
-    console.log(`\n${character2.NOME} venceu a corrida! Parabéns! 🏆`);
-  else console.log("\nA corrida terminou em empate!");
+    console.log(`\n${character2.NOME} venceu a corrida! Parabéns! 🏆`.toUpperCase());
+  else console.log("\nA corrida terminou em empate!".toUpperCase());
 }
 
 (async function main() {
