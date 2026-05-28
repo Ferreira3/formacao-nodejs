829df2d883f423a26ac6728f46ad29df7a797a82
J. Eduardo Ferreira <jeduardoferreira.03@gmail.com>
2026-05-28 19:11:54 -0300

    feat: implement API for drivers and teams with Fastify, add data files and update configuration

--- 05-projeto-formula1/dist/src/server.js
+++ 05-projeto-formula1/dist/src/server.js
@@ -1,2 +1,60 @@
 "use strict";
-console.log("hello world");
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fastify_1 = __importDefault(require("fastify"));
+const cors_1 = __importDefault(require("@fastify/cors"));
+const drivers_json_1 = __importDefault(require("../data/drivers.json"));
+const teams_json_1 = __importDefault(require("../data/teams.json"));
+// Iniciando o servidor, definindo cors e port
+const server = (0, fastify_1.default)({ logger: true });
+const PORT = Number(process.env.PORT);
+server.register(cors_1.default, {
+    origin: '*'
+});
+server.listen({ port: PORT }, () => {
+    console.log(`Server initiated on port ${PORT}!`);
+});
+// Rotas da API
+// Rota dos pilotos
+server.get("/drivers", (request, response) => __awaiter(void 0, void 0, void 0, function* () {
+    response.type("application/json").code(200);
+    return { drivers: drivers_json_1.default };
+}));
+// Rota filtrar piloto
+server.get("/drivers/:id", (request, response) => __awaiter(void 0, void 0, void 0, function* () {
+    const id = parseInt(request.params.id);
+    const driver = drivers_json_1.default.find(d => d.id === id);
+    if (!driver) {
+        response.type("application/json").code(404);
+        return { message: "Driver Not Found" };
+    }
+    response.type("application/json").code(200);
+    return { driver };
+}));
+// Rota dos times
+server.get("/teams", (request, response) => __awaiter(void 0, void 0, void 0, function* () {
+    response.type("application/json").code(200);
+    return { teams: teams_json_1.default };
+}));
+// Rota filtar time
+server.get("/teams/:id", (request, response) => __awaiter(void 0, void 0, void 0, function* () {
+    const id = parseInt(request.params.id);
+    const team = teams_json_1.default.find(t => t.id === id);
+    if (!team) {
+        response.type("application/json").code(404);
+        return { message: "Team Not Found" };
+    }
+    response.type("application/json").code(200);
+    return { team };
+}));
--- 05-projeto-formula1/src/server.ts
+++ 05-projeto-formula1/src/server.ts
@@ -1,1 +1,59 @@
-console.log("hello world");
+import fastify from "fastify";
+import cors from "@fastify/cors";
+import drivers from "../data/drivers.json"
+import teams from "../data/teams.json"
+import { IdParams } from "./utils/interfaces"
+
+// Iniciando o servidor, definindo cors e port
+const server = fastify({logger: true});
+const PORT = Number(process.env.PORT);
+
+server.register(cors, {
+  origin: '*'
+});
+
+server.listen({port: PORT}, () => {
+  console.log(`Server initiated on port ${PORT}!`);
+});
+
+// Rotas da API
+
+// Rota dos pilotos
+server.get("/drivers", async (request, response) => {
+  response.type("application/json").code(200);
+  return { drivers };
+});
+
+// Rota filtrar piloto
+server.get<{Params: IdParams }>("/drivers/:id", async (request, response) => {
+  const id = parseInt(request.params.id);
+  const driver = drivers.find(d => d.id === id);
+
+  if (!driver) {
+    response.type("application/json").code(404);
+    return { message: "Driver Not Found" }
+  }
+
+  response.type("application/json").code(200);
+  return { driver };
+});
+
+// Rota dos times
+server.get("/teams", async (request, response) => {
+  response.type("application/json").code(200);
+  return { teams };
+});
+
+// Rota filtar time
+server.get<{Params: IdParams}>("/teams/:id", async (request, response) => {
+  const id = parseInt(request.params.id);
+  const team = teams.find(t => t.id === id);
+
+  if (!team) {
+    response.type("application/json").code(404);
+    return { message: "Team Not Found" };
+  }
+
+  response.type("application/json").code(200);
+  return { team };
+})
