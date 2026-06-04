f3af73de01888b91449df1fa0c6acaece44a91b4
J. Eduardo Ferreira <jeduardoferreira.03@gmail.com>
2026-06-04 00:03:14 -0300

    feat: enhance fighters API with CRUD operations and detailed fighter stats

--- 06-projeto-ufc/dist/app.js
+++ 06-projeto-ufc/dist/app.js
@@ -60,14 +60,80 @@ var import_express = require("express");
 
 // src/repositories/fighters-repository.ts
 var database = [
-  { id: 1, name: "alex pereira" },
-  { id: 2, name: "cyril gane" },
-  { id: 3, name: "charles oliveira" },
-  { id: 4, name: "ilia topuria" },
-  { id: 5, name: "carlos prates" }
+  {
+    id: 1,
+    name: "Alex Pereira",
+    stats: {
+      record: { w: 13, l: 2, d: 0 },
+      division: "Light Heavyweight",
+      nickname: "Poatan",
+      age: 38
+    }
+  },
+  {
+    id: 2,
+    name: "Ciryl Gane",
+    stats: {
+      record: { w: 12, l: 2, d: 0 },
+      division: "Heavyweight",
+      nickname: "Bon Gamin",
+      age: 36
+    }
+  },
+  {
+    id: 3,
+    name: "Charles Oliveira",
+    stats: {
+      record: { w: 35, l: 10, d: 0 },
+      division: "Lightweight",
+      nickname: "Do Bronx",
+      age: 36
+    }
+  },
+  {
+    id: 4,
+    name: "Ilia Topuria",
+    stats: {
+      record: { w: 16, l: 0, d: 0 },
+      division: "Featherweight",
+      nickname: "El Matador",
+      age: 29
+    }
+  },
+  {
+    id: 5,
+    name: "Carlos Prates",
+    stats: {
+      record: { w: 21, l: 6, d: 0 },
+      division: "Welterweight",
+      nickname: "The Nightmare",
+      age: 32
+    }
+  }
 ];
-var getListFighters = () => __async(null, null, function* () {
+var getAllFighters = () => __async(null, null, function* () {
   return database;
+});
+var getFighterById = (id) => __async(null, null, function* () {
+  return database.find((player) => player.id === id);
+});
+var insertFighter = (fighter) => __async(null, null, function* () {
+  database.push(fighter);
+});
+var deleteOneFighter = (id) => __async(null, null, function* () {
+  const index = database.findIndex((fighter) => fighter.id === id);
+  if (index !== -1) {
+    database.splice(index, 1);
+    return true;
+  }
+  return false;
+});
+var findAndModifyFighter = (id, stats) => __async(null, null, function* () {
+  const index = database.findIndex((fighter) => fighter.id === id);
+  if (index !== 1) {
+    database[index].stats = stats;
+  }
+  return database[index];
 });
 
 // src/utils/http-helper.ts
@@ -83,10 +149,24 @@ var noContent = () => __async(null, null, function* () {
     body: null
   };
 });
+var badRequest = () => __async(null, null, function* () {
+  return {
+    statusCode: 400,
+    body: null
+  };
+});
+var created = () => __async(null, null, function* () {
+  return {
+    statusCode: 201,
+    body: {
+      message: "successful"
+    }
+  };
+});
 
 // src/services/fighters-services.ts
-var getFighterService = () => __async(null, null, function* () {
-  const data = yield getListFighters();
+var getFightersService = () => __async(null, null, function* () {
+  const data = yield getAllFighters();
   let response = null;
   if (data) {
     response = yield ok(data);
@@ -95,16 +175,75 @@ var getFighterService = () => __async(null, null, function* () {
   }
   return response;
 });
+var getFighterByIdService = (id) => __async(null, null, function* () {
+  const data = yield getFighterById(id);
+  let response = null;
+  if (data) {
+    response = yield ok(data);
+  } else {
+    response = yield noContent();
+  }
+  return response;
+});
+var createFighterService = (fighter) => __async(null, null, function* () {
+  if (!fighter || Object.keys(fighter).length === 0) {
+    return yield noContent();
+  }
+  const isValid = typeof fighter.id === "number" && typeof fighter.name === "string";
+  if (!isValid) {
+    return yield badRequest();
+  }
+  yield insertFighter(fighter);
+  return yield created();
+});
+var deleteFighterByIdService = (id) => __async(null, null, function* () {
+  const isDeleted = yield deleteOneFighter(id);
+  if (isDeleted) {
+    return yield ok({ message: "deleted" });
+  } else {
+    return yield badRequest();
+  }
+});
+var updateFighterService = (id, stats) => __async(null, null, function* () {
+  const data = yield findAndModifyFighter(id, stats);
+  const response = yield ok(data);
+  return response;
+});
 
 // src/controllers/fighters-controller.ts
 var getFighters = (req, res) => __async(null, null, function* () {
-  const httpResponse = yield getFighterService();
+  const httpResponse = yield getFightersService();
+  res.status(httpResponse.statusCode).json(httpResponse.body);
+});
+var getFighterById2 = (req, res) => __async(null, null, function* () {
+  const id = Number(req.params.id);
+  const httpResponse = yield getFighterByIdService(id);
+  res.status(httpResponse.statusCode).json(httpResponse.body);
+});
+var postFighter = (req, res) => __async(null, null, function* () {
+  const bodyValue = req.body;
+  const httpResponse = yield createFighterService(bodyValue);
+  res.status(httpResponse.statusCode).json(httpResponse.body);
+});
+var deleteFighterById = (req, res) => __async(null, null, function* () {
+  const id = Number(req.params.id);
+  const httpResponse = yield deleteFighterByIdService(id);
+  res.status(httpResponse.statusCode).json(httpResponse.body);
+});
+var updateFighterById = (req, res) => __async(null, null, function* () {
+  const id = Number(req.params.id);
+  const bodyValue = req.body;
+  const httpResponse = yield updateFighterService(id, bodyValue);
   res.status(httpResponse.statusCode).json(httpResponse.body);
 });
 
 // src/routes.ts
 var router = (0, import_express.Router)();
-router.get("/fighters/list", getFighters);
+router.get("/fighters", getFighters);
+router.get("/fighters/:id", getFighterById2);
+router.post("/fighters", postFighter);
+router.delete("/fighters/:id", deleteFighterById);
+router.patch("/fighters/:id", updateFighterById);
 var routes_default = router;
 
 // src/app.ts
